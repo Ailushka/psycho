a05f001353cd420625dc7567af0ad27c0c1eb1b6
Yulia Okhotina <y.chaser@yandex.ru>
2023-01-04 00:15:08 +0300

    Mobile menu

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+const burgerButton = document.querySelector('.burger');
+const nav = document.querySelector('.nav');
+const menu = document.querySelector('.nav-list');
+const menuLinks = document.querySelectorAll('.nav-list__link');
+
+burgerButton.addEventListener("click", function () {
+    burgerButton.classList.toggle("burger_active");
+    nav.classList.toggle("nav_opened");
+    nav.classList.toggle("transition");
+    menu.classList.toggle("nav-list_mobile");
+    document.querySelector('.page').classList.toggle('no-scroll');
+});
+
+menuLinks.forEach(menuLink => {
+  menuLink.addEventListener('click', () => {
+    burgerButton.classList.remove("burger_active");
+    nav.classList.remove("nav_opened");
+    nav.classList.remove("transition");
+    menu.classList.remove("nav-list_mobile");
+    document.querySelector('.page').classList.remove('no-scroll');
+  })
+});
+
+
+
 /* -------------------- */
 /*     Smooth links     */
 /* -------------------- */
