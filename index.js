1a3ae586f598b048b18ad2ad1b2489471fcebd2c
Yulia Okhotina <y.chaser@yandex.ru>
2023-03-26 00:48:02 +0300

    Added diplomas full-size popup

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/* -------------------- */
+/*      Mobile menu     */
+/* -------------------- */
+
 const burgerButton = document.querySelector('.burger');
 const nav = document.querySelector('.nav');
 const menu = document.querySelector('.nav-list');
@@ -41,3 +45,69 @@ for (let smoothLink of smoothLinks) {
         });
     });
 };
+
+/* -------------------- */
+/*         Popup        */
+/* -------------------- */
+
+const closeButtons = document.querySelectorAll('.button_type_close');
+const diplomasPopup = document.querySelector('.popup_type_diplomas');
+const diplomaImageName = diplomasPopup.querySelector('.diplomas-image');
+const diplomaImageLink = diplomasPopup.querySelector('.diplomas-image');
+const diplomas = document.querySelectorAll('.diplomas-list__item');
+const ESCAPE = 27;
+
+function openPopUp(popup) {
+  popup.classList.add('popup_opened');
+  popup.classList.add('transition');
+  document.querySelector('.page').classList.add('no-scroll');
+  document.addEventListener('click', closePopUpByOverlay);
+  document.addEventListener('keydown', closePopUpByEsc);
+
+}
+
+function closePopUp(popup) {
+  popup.classList.remove('popup_opened');
+  popup.classList.remove('transition');
+  document.querySelector('.page').classList.remove('no-scroll');
+  document.removeEventListener('click', closePopUpByOverlay);
+  document.removeEventListener('keydown', closePopUpByEsc);
+}
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+
+function closePopUpByOverlay(evt) {
+  if (evt.target.classList.contains('popup_opened')) {
+    closePopUp(evt.target);
+  }
+}
+
+function closePopUpByEsc(evt) {
+  const popupOpened = document.querySelector('.popup_opened');
+  if (evt.keyCode === ESCAPE) {
+    closePopUp(popupOpened);
+  }
+}
+
+diplomas.forEach((item) => {
+  item.addEventListener('click', (evt) => {
+    const diplomaToOpen = evt.target.closest('.diplomas-list__item');
+
+    const openedDiplomaImageName = diplomaToOpen.querySelector('.slide__image').alt;
+    const openedDiplomaImageLink = diplomaToOpen.querySelector('.slide__image').src;
+
+    diplomaImageName.alt = openedDiplomaImageName;
+    diplomaImageLink.src = openedDiplomaImageLink;
+
+      openPopUp(diplomasPopup);
+  });
+});
+
+closeButtons.forEach((item) => {
+  item.addEventListener('click', (evt) => {
+      const popUpToClose = evt.target.closest('.popup');
+      closePopUp(popUpToClose);
+  });
+});
